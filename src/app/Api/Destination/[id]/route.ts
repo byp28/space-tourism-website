c87d1cb201f020db1712a4b3165a5fb4d9d547f2
byp28 <yannight28@gmail.com>
2025-04-07 15:25:52 +0200

    02

--- src/app/Api/Destination/[id]/route.ts
+++
@@ -1,8 +0,0 @@
-import * as data from '../../data.json';
-
-export async function GET(_request : Request, {params}:
-    {params : {id : string}}
-){
-    const destination = data.destinations[parseInt(params.id)]
-    return Response.json(destination)
-}
